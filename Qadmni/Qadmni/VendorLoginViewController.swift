701ff917c2aeb3e7404a839c2c9879c7564d7867
Prakash-08 <prakash@Anands-MacBook-Pro.local>
2017-01-19 19:13:25 +0530

    User Login Screen Done
    User Login Screen Done

---
+++ Qadmni/Qadmni/VendorLoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  VendorLoginViewController.swift
+//  Qadmni
+//
+//  Created by Prakash Sabale on 19/01/17.
+//  Copyright © 2017 Qadmni. All rights reserved.
+//
+
+import UIKit
+
+class VendorLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
